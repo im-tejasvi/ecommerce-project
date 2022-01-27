9e5d6493d46a3e221e5f6d8f033de3da41925a61
im-tejasvi <imtejasvi@gmail.com>
2022-01-27 19:22:45 +0530

    Added product-list component and product class

---
+++ frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  sku!: string;
+  name!: string;
+  description!: string;
+  unitPrice!: string;
+  imageUrl!: string;
+  active!: boolean;
+  unitsInStock!: number;
+  dateCreated!: Date;
+  lastUpdated!: Date;
+}
